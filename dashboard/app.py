efa4d96c50f8dbe3dad4e089f8970cc8fad8b094
sulakshanamanthri1 <23b81a7251@cvr.ac.in>
2026-06-08 10:07:16 +0530

    retrained model

--- dashboard/app.py
+++ dashboard/app.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_PATH = BASE_DIR / "data" / "tesla_deliveries_dataset_2015_2025.csv"
 
-MODEL_PATH = BASE_DIR / "models" / "best_model.pkl"
+MODEL_PATH = BASE_DIR / "models" / "best_tuned_model.pkl"
 
 st.set_page_config(
     page_title="Tesla Dashboard",
